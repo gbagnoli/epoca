c2e9cd5c9e0737b84b0d47b7f5bb8a65583a6747
Giacomo Bagnoli <gbagnoli@gmail.com>
2013-01-18 13:04:45 +0100

    Styled navbar

--- client/root.js
+++ client/root.js
@@ -9,10 +9,17 @@ Template.root.currentUser = function() {
     return displayName(Meteor.user());
 }
 
+Template.root.isLoggedIn = function() {
+    var user = Meteor.user();
+    if (!user)
+        return false;
+    return true;
+};
+
 Template.root.events({
-'click button' : function () {
-  // template data, if any, is available in 'this'
-  if (typeof console !== 'undefined')
-    console.log("You pressed the button");
-}
+    'click button' : function () {
+      // template data, if any, is available in 'this'
+      if (typeof console !== 'undefined')
+        console.log("You pressed the button");
+    }
 });
--- client/router.js
+++ client/router.js
@@ -1,4 +1,28 @@
 Meteor.Router.add({
-    "/": "root"
-})
+    "/": "root",
+    "/characters": "characters",
+    "/characters/:id": function(id) {
+        Session.set("characterId", id);
+        return "character";
+    }
+});
 
+Meteor.Router.filters({
+    requireLogin: function(page) {
+        var user = Meteor.user();
+        if (user) {
+            return page;
+        } else {
+            return "root";
+        }
+    }
+});
+
+Meteor.Router.filter("requireLogin", {except: "root"});
+
+Template.navbar.isPageActive = function (pagename) {
+    var page = Meteor.Router.page();
+    if (page == pagename)
+        return "active";
+    return "";
+};
