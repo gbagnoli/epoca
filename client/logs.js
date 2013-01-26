d0255c9af3133588fc2c3e300d0a1d30b9238ba7
Giacomo Bagnoli <gbagnoli@gmail.com>
2013-01-26 14:11:54 +0100

    Added button to clear all logs

--- client/logs.js
+++ client/logs.js
@@ -2,6 +2,10 @@ Template.logs.logs = function () {
     return Logs.find();
 };
 
+Template.logs.anyLog= function () {
+    return Logs.find().count() > 0;
+};
+
 Template.log.ownerName = function () {
     return displayName(this.user);
 };
@@ -21,5 +25,16 @@ Template.log.events({
         node = $(node);
         var log_id = node.attr('log');
         Logs.remove({"_id": log_id});
-    }
+    },
 });
+
+Template.logs.events({
+    'click #remove-all-logs': function(event) {
+        bootbox.confirm("Sicuro di voler rimuovere tutti i log?",
+                        function (result) {
+                            if (result) {
+                                Logs.remove({});
+                            }
+                        });
+    }
+})
